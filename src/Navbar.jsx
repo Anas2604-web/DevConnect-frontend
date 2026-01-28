1cfb7d36b8555e83b1b767dc27770a62ebacb8e6
Anas Khan <annaasskhan6@gmail.com>
2026-01-28 15:41:13 +0530

    Subscribed to the store and displayed changes in ui

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,35 +1,48 @@
-const Navbar = () => {
-    return (
-    <div className="navbar bg-base-300 text-base-content shadow-md px-6">
+import { useSelector } from "react-redux";
 
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl"> 👨🏻‍💻 DevConnect</a>
-  </div>
-  <div className="flex gap-2">
-    <div className="dropdown dropdown-end mx-6">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
+const Navbar = () => {
+  const user = useSelector((store) => store.user);
+
+  return (
+    <div className="navbar bg-base-300 text-base-content shadow-md px-6">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">👨🏻‍💻 DevConnect</a>
       </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
+
+      <div className="flex items-center gap-3">
+
+        {!user && <p>Login </p>}
+
+        {user && <p className="font-medium">Welcome {user.firstName}</p>}
+
+        {user && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="user photo"
+                  src={user.photoUrl || "https://i.pravatar.cc/100"}
+                />
+              </div>
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+            >
+              <li><a>Profile</a></li>
+              <li><a>Settings</a></li>
+              <li><a className="text-error">Logout</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-</div>   
-    )
-}
+  );
+};
 
 export default Navbar;
