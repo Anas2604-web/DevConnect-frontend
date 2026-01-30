fae6a8c425be5eced263f6e798004949c666f3cc
Anas Khan <annaasskhan6@gmail.com>
2026-01-30 22:00:45 +0530

    Created feedSlice and Feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,34 @@
+import axios from "axios";
+import { URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+
 const Feed = () => {
+    const dispatch = useDispatch();
+    
+    const feed = useSelector((store => store.feed));
+
+    const getFeed = async () => {
+        if(feed) return;
+        try {
+         const res = await axios.get(URL + "/feed", 
+          {withCredentials:true}
+     )
+        dispatch(addFeed(res.data));
+     }
+     catch(err) {
+        //
+     }
+    }
+    useEffect(() => {
+        getFeed();
+    }, [])
+
     return (
-        <>
-        Feed
-        </>
+        <div>
+           Feed
+        </div>
     )
 }
 
