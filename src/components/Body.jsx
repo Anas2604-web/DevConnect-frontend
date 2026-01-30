2262fcfdbbda900d28492fe7ac9beb3ebc1cc737
Anas Khan <annaasskhan6@gmail.com>
2026-01-30 21:29:10 +0530

    Protected Routes(public and private) and persist login on refresh

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { URL } from "../utils/constants";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
+  const user = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(
+        URL + "/profile/view",
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+    } catch (err) {
+       // nothing here
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      fetchUser();
+    }
+  }, [user]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
